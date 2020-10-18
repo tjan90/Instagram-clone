2fa17156d257fdd2e943f042aeb8b95f08c77abf
Tanveer Jan <tjan90@yahoo.com>
2020-10-18 13:09:05 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,6 @@
 import firebase from 'firebase'
 
-const firebaseConfig = firebase.initializeApp({
-    apiKey: "AIzaSyAhpbTjlXfich9QEX0YR0kOylL73-7Ve_o",
-    authDomain: "instagram-clone-a1669.firebaseapp.com",
-    databaseURL: "https://instagram-clone-a1669.firebaseio.com",
-    projectId: "instagram-clone-a1669",
-    storageBucket: "instagram-clone-a1669.appspot.com",
-    messagingSenderId: "1014286536030",
-    appId: "1:1014286536030:web:6fd5231a2e6a9d8ffbd3cf"
-  });
+const firebaseConfig = "config here
 
   const db = firebaseConfig.firestore();
   const auth =  firebase.auth();
